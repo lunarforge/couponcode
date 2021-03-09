a34ab94455da5992a658b673318d689f5a758519
windhooked <hannesdewaal@gmail.com>
2021-03-09 10:58:37 +0200

    generate with hashids

--- cmd/voucher_generate/main.go
+++ cmd/voucher_generate/main.go
@@ -3,29 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"strconv"
 
-	"github.com/lunarforge/couponcode/pkg/voucher"
+	"github.com/speps/go-hashids"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatalf("usage: %v seed", os.Args[0])
+		log.Fatalf("usage: %v salt data", os.Args[0])
 	}
-	seedInt, err := strconv.ParseInt(os.Args[1], 10, 64)
+	data, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		log.Fatalf("%v", err)
 	}
 
-	rand.Seed(seedInt)
-	cc := voucher.New(3, 5)
-	code := cc.Generate()
-	validated, err := cc.Validate(code)
-	if err != nil {
-		log.Fatalf("%v %v %v\n", code, validated, err)
-	}
-	fmt.Printf("%v", code)
+	hd := hashids.NewData()
+	hd.Salt = os.Args[1]
+	hd.MinLength = 12
+	//hd.Alphabet = "abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXYZ2346789"
+	hd.Alphabet = "ABCDEFGHJKLMNPQRTUVWXYZ2346789"
+	h, _ := hashids.NewWithData(hd)
+	e, _ := h.Encode([]int{data})
+	fmt.Println(e)
 
+	//d, _ := h.DecodeWithError(e)
+	//fmt.Printf("%v-%v-%v %v", e[:4], e[4:8], e[8:], d)
 }
